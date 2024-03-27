8c0a1ae6bb7d5551b851c8450d1ad8f72148ba66
john7319 <jonniekaras@gmail.com>
2024-03-27 18:16:00 +0300

    Removed unnecessary comments

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 // Write your code here!
-/*const element = document.createElement("div");
-document.body.append(element);
 
-const ul = document.createElement("ul");
-document.body.append(ul);
-
-for (let i=0; i<3; i++){
-    const li = document.createElement("li");
-    li.textContent = ( i + 1 ).toString();
-    ul.append(li);
-}
-*/
 document.getElementById('main').remove()
 
 const newHeader = document.createElement("h1");
